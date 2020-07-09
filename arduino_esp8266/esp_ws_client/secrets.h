dc313d6da40b31b0357ef06af87202ac07bfed34
anuvgupta <me@anuv.me>
2020-07-08 22:51:22 -0700

    Update secrets.h

--- arduino_esp8266/esp_ws_client/secrets.h
+++ arduino_esp8266/esp_ws_client/secrets.h
@@ -1,5 +1,5 @@
 #define SECRET_SSID "ssid"
 #define SECRET_PASS "pass"
-#define SECRET_AUTH_JSON "{\"event\":\"auth\",\"data\":{\"password\":\"password\"}}"
+#define SECRET_AUTH_JSON "{\"event\":\"auth\",\"data\":{\"password\":\"control\"}}"
 #define DEVICE_SYNC_JSON "{\"event\":\"arduino_sync\",\"data\":\"device1\"}"
-char secret_ssid[9] = SECRET_SSID;
+char secret_ssid[11] = SECRET_SSID;
